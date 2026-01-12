2e46450ed301933906b9874e860938b8b6531199
Lucia Alexandra Little <73139437+chufakiks@users.noreply.github.com>
2026-01-12 10:58:06 +0100

    Update backend URL to Codespaces

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://api.sign2speech.com',
+  backendUrl: 'https://shiny-space-carnival-j96wgpv55gr3qw6x-8000.app.github.dev',
 };
